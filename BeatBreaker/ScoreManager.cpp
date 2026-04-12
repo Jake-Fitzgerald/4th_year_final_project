bd0af778cd327de769b049f836bf63507c223c45
Jake Fitzgerald <115044918+Jake-Fitzgerald@users.noreply.github.com>
2026-04-12 22:00:53 +0100

    Scoremanager saving and loading implemented.

--- BeatBreaker/ScoreManager.cpp
+++ BeatBreaker/ScoreManager.cpp
@@ -1,1 +1,94 @@
 #include "ScoreManager.h"
+
+ScoreManager::ScoreManager()
+{
+}
+
+void ScoreManager::loadScores()
+{
+	std::ifstream file(m_filePath);
+	if (!file.is_open())
+	{
+		std::cerr << "[Score Manager] No scores file found" << std::endl;
+		return;
+	}
+
+	m_scoreVec.clear();
+
+	std::string currentLine;
+	while (std::getline(file, currentLine))
+	{
+		// Skip blank lines
+		//if (line.empty())
+		//{
+		//	continue;
+		//}
+
+		int pos = -1;
+
+		for (int i = 0; i < currentLine.length(); i++)
+		{
+			if (currentLine[i] == '=')
+			{
+				pos = i;
+				break;
+			}
+		}
+
+		// Skip shoddy lines
+		if (pos == -1)
+		{
+			continue;
+		}
+
+		// Split name and score
+		std::string name = currentLine.substr(0, pos);
+		std::string amount = currentLine.substr(pos + 1);
+
+		int value = std::stoi(amount);
+
+		SongScore entry;
+		entry.songName = name;
+		entry.score = value;
+		m_scoreVec.push_back(entry);
+
+		std::cerr << "[Score Manager] Loaded scores" << name << " = " << value << std::endl;
+	}
+
+	file.close();
+}
+
+void ScoreManager::saveScores()
+{
+	std::ofstream file(m_filePath);
+
+	if (!file)
+	{
+		std::cerr << "[Score Manager] Couldn't open score text file for saving" << std::endl;
+	}
+
+	for (int i = 0; i < m_scoreVec.size(); i++)
+	{
+		file << m_scoreVec[i].songName << "=" << m_scoreVec[i].score << "\n";
+	}
+
+	file.close();
+}
+
+int ScoreManager::getPB(std::string& t_songName)
+{
+	for (int i = 0; i < m_scoreVec.size(); i++)
+	{
+		if (m_scoreVec[i].songName == t_songName)
+		{
+			return m_scoreVec[i].score;
+		}
+	}
+
+	return 0;
+}
+
+bool ScoreManager::updatePB(std::string t_songName, int t_score)
+{
+	return false;
+}
